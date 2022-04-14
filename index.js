a72fc9bcb1d2e61f577dfe0b73c006da8b54f24f
Arelial ID <98020400+4relial@users.noreply.github.com>
2022-04-14 12:59:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ var app = express();
 createConnection.then(sock => {
     sock.ev.on('messages.upsert', async m => await core(sock, m))	
 })
-<<<<<<< HEAD
+
 app.listen(process.env.PORT || 8080);
 createConnection.catch(erro => console.log(erro, 'Conn'))
-=======
 
-worker:  node index.js
-
-createConnection.catch(erro => console.log(erro, 'Conn'))
->>>>>>> 9dac8812f9c8388a9d3647419aece7b1b2104540
