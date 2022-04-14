17fa57468c2c19fd1326cd8dc9427d89b9f0ad3c
4relial <ilham.a.pahmi@gmail.com>
2022-04-14 12:41:22 +0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { createConnection } = require('./connection')
 const { core } = require('./lib');
+var express = require('express');
+var app = express();
 
 createConnection.then(sock => {
     sock.ev.on('messages.upsert', async m => await core(sock, m))	
 })
-
+app.listen(process.env.PORT || 8080);
 createConnection.catch(erro => console.log(erro, 'Conn'))
